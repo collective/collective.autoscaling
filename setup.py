da56f24a6cfb07d42c918719995c78358e749b0f
Laurent Lasudry <lasudry@gmail.com>
2016-04-11 15:20:00 +0200

    Add Plone 5 classifier

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",
